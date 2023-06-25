f56627a6db2cfe2706fa05b071195b10363af70b
Dominic <dominicwong09@gmail.com>
2023-06-26 10:52:26 +1200

    updated content

--- src/content/intro.jsx
+++ src/content/intro.jsx
@@ -1,7 +1,7 @@
 export const introContent = {
     occupation: 'Software Engineer',
 
-    pOne: "I'm a career transitioner with a background in Account Management and Technical Operations in Ad-Tech. In this role, I discovered coding and glimpsed it's elegance of doing things efficiently.",
+    pOne: "I'm a career transitioner with a background in Tecnnical Operations and Account Management in Ad-Tech. In this role, I discovered coding and glimpsed it's elegance of doing things efficiently.",
 
     pTwo: "This led me to a Software Engineer course with General Assembly, which I completed recently. It was a rewarding experience where I learnt a lot but at the same time, humbled by how I'm only just scratching the surface.",
     
