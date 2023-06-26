dfe2ac6ef01e2ca586b20bd326b9c5403ad3cf8b
Dominic <dominicwong09@gmail.com>
2023-06-26 18:20:10 +1200

    re-did skills page and fixed email submission bug

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,37 +1,23 @@
 import React from 'react';
-import { skills, currentlyLearning } from '../content/skills';
+import Languages from './languages';
+import Frameworks from './frameworks';
+import Tools from './tools';
 
 const Skills = () => {
-
-    const generateSkills = () => {
-        return Object.keys(skills).map( ( heading ) => {
-            return (
-                <div key={heading} className="skill">
-                    <h2 className="skill-h-size">{heading}</h2>
-                    <ul>
-                        { skills[heading].map( (skill) => {
-                            return (
-                                <li key={skill}>{skill}</li>
-                            )
-                        })}
-                    </ul>
-                </div>
-            );
-        });
-    };
 
     return (
         <div id="skills">
             
             <div className="skills-list">
-                { generateSkills() }
+                <Languages />
+                <Frameworks />
+                <Tools />
             </div>
 
             <div className="learning">
                 {/* <p>{ currentlyLearning.text }</p> */}
                 <p className="big-text">See below for projects I've worked on.</p>
             </div>
-
 
         </div>
     )
--- src/content/skills.jsx
+++ src/content/skills.jsx
@@ -1,9 +1,3 @@
-export const skills = {
-    Languages: ['○ Javascript', '○ Ruby', '○ HTML5', '○ CSS3', '○ SQL'], // Add SQL
-    Frameworks: ['○ React', '○ Rails', '○ Gatsby', '○ Tailwind'],
-    Tools: ['○ Git / Github', '○ PostgreSQL', '○ Notion', '○ Figma'],
-}
-
 export const currentlyLearning = {
     text: "Currently, I'm interested in learning / am learning GraphQL, Go and ThreeJs. I also plan to complete the AWS certificate."
 }
